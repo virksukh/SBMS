0321edefd5e2d36288430768b35fe95478b0cadc
Sukhdeep Virk <sukh.code@gmail.com>
2024-11-13 17:13:37 -0500

    1st Boot app

---
+++ 01-SpringBoot-app/src/main/java/in/india/Application.java
@@ -0,0 +1,23 @@
+package in.india;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.lang.model.element.NestingKind;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String msg(){
+        return "Hello world";
+    }
+}
