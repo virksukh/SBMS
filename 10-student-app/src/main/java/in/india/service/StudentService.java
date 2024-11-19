94d11126917dfd7048c083141b30b0de47fe2b66
Sukhdeep Virk <sukh.code@gmail.com>
2024-11-18 19:40:38 -0500

    web app

---
+++ 10-student-app/src/main/java/in/india/service/StudentService.java
@@ -0,0 +1,8 @@
+package in.india.service;
+import in.india.entity.Student;
+import java.util.List;
+
+public interface StudentService {
+    public boolean addStudent(Student student);
+    public List<Student> getAllStudents();
+}
