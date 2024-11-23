531ec0a8c78a88faf52dc7d5a2cd91608c064822
Sukhdeep Virk <sukh.code@gmail.com>
2024-11-23 13:41:39 -0500

    rest api

--- 02-Rest-api/src/main/java/in/india/controller/ProductRestConroller.java
+++ 02-Rest-api/src/main/java/in/india/controller/ProductRestConroller.java
@@ -2,6 +2,7 @@ package in.india.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductRestConroller {
 
     @GetMapping("/product")
-    public ResponseEntity<String> getProducts(@RequestParam("id") Integer id,@RequestParam("name") String name) {
+    public ResponseEntity<String> getProducts(@RequestParam("id") Integer id) {
         String msg = "";
 
         if(id==101){
@@ -18,6 +19,20 @@ public class ProductRestConroller {
             msg="Apple costs 1 lacs";
         }else{
             msg="no product found";
+        }
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<String> getProductsPrice(@PathVariable("id") Integer id) {
+        String msg = "";
+
+        if(id==101){
+            msg="Samsung costs 2 lacs";
+        }else if(id==102){
+            msg="Samsung costs 1 lacs";
+        }else{
+            msg="no Samsung product found";
         }
         return new ResponseEntity<>(msg, HttpStatus.OK);
     }
